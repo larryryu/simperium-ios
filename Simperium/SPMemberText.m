a3f23ddd93ca09b8b12851235f5143e47d460534
Lukman Sanusi <larryryu@gmail.com>
2016-10-12 14:02:17 -0500

    use otype to override default OP_STRING for string members
    using otype (which currently supports replace operation) as a way to
    override default behavior of syncing string diff. This allows a replace
    operation on string changes.

--- Simperium/SPMemberText.m
+++ Simperium/SPMemberText.m
@@ -60,6 +60,19 @@ static SPLogLevels logLevel = SPLogLevelsInfo;
         return @{ };
     }
     
+    //If otype is replace then avoid diff
+    if ([self.otype isEqualToString:OP_REPLACE]) {
+        if ([thisValue isEqualToString: otherValue]) {
+            return @{ };
+        }
+        
+        // Construct the diff in the expected format
+        return @{
+                 OP_OP : OP_REPLACE,
+                 OP_VALUE : otherValue
+                 };
+    }
+    
     // DiffMatchPatch: find the diff, and let's use some logic from MobWrite to clean stuff up
     NSMutableArray *diffList = [self.dmp diff_mainOfOldString:thisValue andNewString:otherValue];
     if (diffList.count > 2) {
@@ -86,6 +99,11 @@ static SPLogLevels logLevel = SPLogLevelsInfo;
     // if ([thisValue length] == 0)
     //    return otherValue;
     
+    //If otype is replace then avoid diff
+    if ([self.otype isEqualToString:OP_REPLACE]) {
+        return [otherValue copy];
+    }
+    
     NSMutableArray *diffs   = [self.dmp diff_fromDeltaWithText:thisValue andDelta:otherValue error:error];
     NSMutableArray *patches = [self.dmp patch_makeFromOldString:thisValue andDiffs:diffs];
     NSArray *result         = [self.dmp patch_apply:patches toString:thisValue];
@@ -94,6 +112,12 @@ static SPLogLevels logLevel = SPLogLevelsInfo;
 }
 
 - (NSDictionary *)transform:(id)thisValue otherValue:(id)otherValue oldValue:(id)oldValue error:(NSError **)error {
+    
+    //If otype is replace then avoid applying diff
+    if ([self.otype isEqualToString:OP_REPLACE]) {
+        return [NSDictionary dictionaryWithObjectsAndKeys:OP_REPLACE,OP_OP,thisValue,OP_VALUE, nil];
+    }
+    
     // Calculate the delta from the Ghost to the Local + Remote values. Treat any error here as fatal
     NSMutableArray *thisDiffs       = [self.dmp diff_fromDeltaWithText:oldValue andDelta:thisValue error:error];
     NSMutableArray *otherDiffs      = [self.dmp diff_fromDeltaWithText:oldValue andDelta:otherValue error:error];
